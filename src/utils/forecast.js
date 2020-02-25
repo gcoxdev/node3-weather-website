b2434b7f0f89eb63ceb946c0a008c8076ee2bb15
Gary Cox <gary@gcoxdev.com>
2020-02-24 22:53:50 -0600

    Added forecast data: temp high/low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(body.error)
         } else {
-            const { summary } = body.daily.data[0]
+            const { summary, temperatureHigh, temperatureLow } = body.daily.data[0]
             const { temperature, precipProbability } = body.currently
-            callback(undefined, `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
+            callback(undefined, `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain. Today's temperature is expected to reach a high of ${temperatureHigh} degrees with a low of ${temperatureLow} degrees.`)
         }
     })
 }
